c9c94d8da84da9a3364bb7b76b17a0f519c0a06a
Kelvin Elliott <kelliott11@yahoo.com>
2021-07-03 18:35:37 -0400

    theme update

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,7 +23,7 @@ export default function Post() {
       .catch(console.error);
   }, []);
   return (
-    <main className='bg-green-100 min-h-screen p-12'>
+    <main className='bg-white min-h-screen p-12'>
       <section className='container mx-auto'>
         <h1 className='text-5xl flex justify-center cursive'>Blog Post Page</h1>
         <h2 className='text-lg  text-gray-600 flex justify-center mb-12'>
@@ -35,7 +35,7 @@ export default function Post() {
               <article>
                 <Link to={'/post/' + post.slug.current} key={post.slug.current}>
                   <span
-                    className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400'
+                    className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-gray-400'
                     key={index}
                   >
                     <img
@@ -44,7 +44,7 @@ export default function Post() {
                       className='w-full h-full rounded-r object-cover absolute'
                     />
                     <span className='block relative h-full flex justify-end items-end pr-4 pb-4'>
-                      <h3 className='text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded'>
+                      <h3 className='text-white text-lg font-bold px-3 py-4 bg-gray-700 text-gray-100 bg-opacity-75 rounded'>
                         {post.title}
                       </h3>
                     </span>
