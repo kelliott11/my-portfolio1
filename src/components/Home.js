c4525a3cb63eae1af9000de2218f7ce879776012
Kelvin Elliott <kelliott11@yahoo.com>
2021-07-03 18:02:56 -0400

    entire portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import { React } from 'react';
+import image from '../pic.jpg';
+
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt='Apple Laptop'
+        className='absolute object-cover w-full h-full'
+      />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl text-gray-50 font-bold cursive leading-none lg:leading-snug home-name'>
+          Hello, I'm Kelvin.
+        </h1>
+      </section>
+    </main>
+  );
+}
